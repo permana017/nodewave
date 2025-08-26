f5295f4805f8002f1f0750cd0683b4a68d710888
permana017 <ahdipermana07@gmail.com>
2025-08-26 13:10:03 +0700

    add method options

--- app/api/todo/[id]/route.ts
+++ app/api/todo/[id]/route.ts
@@ -9,22 +9,21 @@ type Params = {
 
 export async function PUT(req: NextRequest, { params }: Params) {
   const token = req.cookies.get("token")?.value;
-  console.log('id', params?.id);
-  
+  console.log("id", params?.id);
+
   if (!token) {
-      return NextResponse.json(
-          { success: false, message: "No token found" },
-          { status: 401 }
-        );
-    }
-    const body = await req.json();
-    console.log('id', body);
+    return NextResponse.json(
+      { success: false, message: "No token found" },
+      { status: 401 }
+    );
+  }
+  const body = await req.json();
   try {
-    const { data } = await axios.put(BE_URL+`/${params?.id}/mark`, body, {
+    const { data } = await axios.put(BE_URL + `/${params?.id}/mark`, body, {
       headers: { Authorization: `Bearer ${token}` },
     });
     return NextResponse.json({ success: true, ...data });
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
     const data = err.response?.data || { message: "Internal error" };
     return NextResponse.json(
@@ -34,8 +33,7 @@ export async function PUT(req: NextRequest, { params }: Params) {
   }
 }
 
-export async function DELETE(req: NextRequest, { 
-  params }: Params) {
+export async function DELETE(req: NextRequest, { params }: Params) {
   const id = params.id;
   const token = req.cookies.get("token")?.value;
   if (!token) {
@@ -49,7 +47,7 @@ export async function DELETE(req: NextRequest, {
       headers: { Authorization: `Bearer ${token}` },
     });
     return NextResponse.json({ success: true, ...data });
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
     const data = err.response?.data || { message: "Internal error" };
     return NextResponse.json(
@@ -57,4 +55,15 @@ export async function DELETE(req: NextRequest, {
       { status: err.response?.status || 500 }
     );
   }
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // atau domain FE kamu
+      "Access-Control-Allow-Methods": "GET,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
